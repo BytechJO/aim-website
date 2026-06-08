5a8f9345d8e20ae44cbec3bf3e218b6a11f2a072
Tareq1333 <developer1@bytechjo.com>
2026-06-08 15:02:33 +0300

    des dashboard v1

--- desktop/main.js
+++ desktop/main.js
@@ -1,23 +1,39 @@
-const { app, BrowserWindow } = require('electron');
-
-const PRODUCTION_URL = 'https://YOUR-APP.netlify.app'; // replace with your Netlify URL
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 800,
+    width: 1380,
+    height: 860,
+    minWidth: 1100,
+    minHeight: 680,
+    backgroundColor: '#08080A',
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
 
-  win.loadURL(PRODUCTION_URL);
+  // Patch CORS headers so fetch() to localhost:3000 works from a file:// origin
+  session.defaultSession.webRequest.onHeadersReceived(
+    { urls: ['http://localhost:3000/*'] },
+    (details, callback) => {
+      callback({
+        responseHeaders: {
+          ...details.responseHeaders,
+          'Access-Control-Allow-Origin': ['*'],
+          'Access-Control-Allow-Headers': ['Content-Type, Authorization'],
+          'Access-Control-Allow-Methods': ['GET, POST, PUT, DELETE, OPTIONS'],
+        },
+      });
+    }
+  );
+
+  win.loadFile(path.join(__dirname, 'dashboard.html'));
 }
 
 app.whenReady().then(() => {
   createWindow();
-
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
