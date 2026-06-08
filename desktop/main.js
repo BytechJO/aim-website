837bf6d7414e1bb7874566a37ca3e0a5292e3fe4
Tareq1333 <developer1@bytechjo.com>
2026-06-08 10:16:20 +0300

    add electron desktop app

---
+++ desktop/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+const PRODUCTION_URL = 'https://YOUR-APP.netlify.app'; // replace with your Netlify URL
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL(PRODUCTION_URL);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
