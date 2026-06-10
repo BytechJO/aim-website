5bd1f3d3960acdf0d2ed7439946dfb9f1ea138fa
waleed-alqadi <developer3@bytechjo.com>
2026-06-10 16:59:24 +0300

    editing

--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -1,17 +1,23 @@
-import { Router } from 'express';
-import { getAll, getOne, create, update, remove } from '../controllers/products.controller';
-import { authenticate } from '../middleware/authenticate';
-import { requireAdmin } from '../middleware/authorize';
+import { Router } from "express";
+import {
+  getAll,
+  getOne,
+  create,
+  update,
+  remove,
+} from "../controllers/products.controller";
+import { authenticate } from "../middleware/authenticate";
+import { requireAdmin } from "../middleware/authorize";
 
 const router = Router();
 
 // approved admin or super_admin only
-router.use(authenticate, requireAdmin);
+router.get("/", getAll);
+router.get("/:slug", getOne);
 
-router.get('/', getAll);
-router.get('/:id', getOne);
-router.post('/', create);
-router.put('/:id', update);
-router.delete('/:id', remove);
+// Protected
+router.post("/", authenticate, requireAdmin, create);
+router.put("/:id", authenticate, requireAdmin, update);
+router.delete("/:id", authenticate, requireAdmin, remove);
 
 export default router;
